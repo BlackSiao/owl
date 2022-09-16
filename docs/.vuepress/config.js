cec4d5aee66638ae62cb346d6e7725f7644c5bfa
BlackHsiao <blackhsiao@qq.com>
2022-09-16 16:44:16 +0800

    2022.9.16

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,13 +9,13 @@ module.exports = {
 
         //导航栏除了返回首页，还应该想点其他的动能
         navbar: [
-            { text: 'Home', link: '/', target:'_self', rel:'' },
-       
+            { text: 'Home', link: '/', target: '_self', rel: '' },
+
         ],
 
         search: true, //禁用搜索栏
         searchMaxSuggestions: 10,
-        
+
         lastUpdated: '最新一次更新时间为：', //显示更新时间
 
         //nextLinks:false,
@@ -24,14 +24,13 @@ module.exports = {
         smoothScroll: true, //启动页面滚动效果
 
         //默认下sidebar只显示当前活动页面的标题，但 displayAllHeaders: true 来设置所有页面的标题链接
-
+        sidebarDepth: 0,
         sidebar: {
             "/salted_fish/": [
                 {
                     title: '来点二次元',
                     path: '/salted_fish/jap/jap_index',
                     collaspable: false,
-                    sidebarDepth: 5,
                     children: [
                         '/salted_fish/jap/来自深渊',
                         '/salted_fish/jap/异世界舅舅'
@@ -39,16 +38,34 @@ module.exports = {
 
                 },
                 {
-                       title: 'steam游戏推荐',
+                    title: 'steam游戏推荐',
                     path: '/salted_fish/game/game_index',
-                    collaspable: false,
-                    sidebarDepth: 5,
+                    collaspable: false,                
                     children: [
-                        '/salted_fish/game/civilization',
-                        '/salted_fish/game/dungeon'
+                        {
+                            title: "Civilization",
+                            path: "/salted_fish/game/civilization"
+                        },
+                        {
+                            title: "Darkest Dungeon",
+                            path: "/salted_fish/game/dungeon"
+                        }
                     ]
 
                 },
+                {
+                    title: '电影',
+                    path: '/salted_fish/movie/movie_index',
+                    collaspable: false,
+                    children: [
+                        {
+                            title: "李安《父亲三部曲》",
+                            path: "/salted_fish/movie/李安"
+                        }
+
+                    ]
+                    
+                }
             ],
             "/rise_up/": [
                 'up-a',
